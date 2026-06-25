6a4a22e5dd38e6995bb7c89cc9b0cb6b8df6cf1f
Juan_Raigosa <raikez630@gmail.com>
2026-06-25 07:11:23 -0500

    se quito el ejs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const clienteController = require ('./controllers/cliente.controller');
 
 const app = express()
-app.set('view engine', 'ejs');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
